aabbce659dddb2bd2c49dcc318f18c2dbf6189e9
benjamin lloyd <benjamin.a.lloyd25@gmail.com>
2022-03-11 11:43:34 -0500

    added local settings

---
+++ music_library_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!c)g$pkz%b8or)1-m5ds7p9t3(-_78&v75u)2fpahd_w_quf(m'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_library_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'JunE@@2717'
+    }
+}
